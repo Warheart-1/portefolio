7cafa65ee5b3f7df310903ad002db9819f2e5f84
Warheart-1 <axel97477@gmail.com>
2022-03-18 12:28:35 +0100

    start project

---
+++ src/composents/Banner.tsx
@@ -0,0 +1,19 @@
+import "../style/Banner.css"
+
+function Banner(){
+    return(
+        <div className="nav-bar">
+            <h1>Porte-Folio</h1>
+            <div className="search-bar">
+                <input type="text" placeholder="Rechercher.."/>
+            </div>
+            <div className="logo">
+                <div className="icon">
+                    Axel SELEBRAN
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/composents/CV.tsx
@@ -0,0 +1,9 @@
+
+function CV (){
+    return (
+        <>
+        </>
+    )
+}
+
+export default CV
