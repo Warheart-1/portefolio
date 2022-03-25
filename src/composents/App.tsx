89cc487bded842a1786688a6331a329dadbc88fc
Warheart-1 <axel97477@gmail.com>
2022-03-25 19:29:43 +0100

    style cv en cours

--- src/composents/App.tsx
+++ src/composents/App.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import logo from '../logo.svg';
 import '../style/App.css';
 import Banner from "./Banner";
 import Intro from "./Intro";
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-
 
 function App() {
   return (
--- src/composents/CV.tsx
+++ src/composents/CV.tsx
@@ -1,8 +1,65 @@
+import "../style/CV.css"
 
-function CV (){
+
+function CV (): JSX.Element {
     return (
         <>
-            
+            <div id="contents">
+                <div className="Progress-bar">
+                    <div id="HTML">
+                        <span className="HTML-title">HTML</span>
+                        <span className="HTML-content">100%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="CSS">
+                        <span className="CSS-title">CSS</span>
+                        <span className="CSS-content">80%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="JS">
+                        <span className="JS-title">JavaScript</span>
+                        <span className="JS-content">60%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="TyepScript">
+                        <span className="TyepScript-title">TypeScript</span>
+                        <span className="TyepScript-content">50%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="jQuery">
+                        <span className="jQuery-title">jQuery</span>
+                        <span className="jQuery-content">50%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="React">
+                        <span className="React-title">React</span>
+                        <span className="React-content">30%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="PHP">
+                        <span className="PHP-title">PHP</span>
+                        <span className="PHP-content">60%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="MySQL">
+                        <span className="MySQL-title">MySQL</span>
+                        <span className="MySQL-content">50%</span>
+                    </div>
+                </div>
+                <div className="Progress-bar">
+                    <div id="Git">
+                        <span className="Git-title">Git</span>
+                        <span className="Git-content">60%</span>
+                    </div>
+                </div>
+            </div>            
         </>
     )
 }
