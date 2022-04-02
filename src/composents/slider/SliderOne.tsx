f30a5f07853f37e5e05b57d7b01a0950e1ce5ea7
Warheart-1 <axel97477@gmail.com>
2022-04-02 23:18:33 +0200

    style du slider en cours

--- src/composents/slider/SliderOne.tsx
+++ src/composents/slider/SliderOne.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components"
+import styled from "styled-components" 
+import "../../style/Slider.css"      
 
 const SliderContainer = styled.div`
     display: flex;
@@ -13,19 +14,47 @@ const SliderTextWapper = styled.div`
     justify-content: center;
     margin : 0px 10px;
     `
+
+const SliderTextTitle = styled.h1`
+    text-align: center;
+    font-size: 50px;
+    font-weight: bold;
+    display: none;
+    `
+
+const SliderImg = styled.div`
+    width: 1000px;
+    height: 500px;
+    background-image: url("https://cdn.discordapp.com/attachments/538463475765215306/959851259937050714/Screenshot_2022-04-02_175309.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: opacity 0.4s ease-in-out; 
+    &:hover{
+        ${SliderTextTitle}{
+            display: block;
+        }
+        opacity: 0.5; 
+    }        
+    `
+
+
+
 function SlideOne():JSX.Element{
     return(
         <SliderContainer>
             <SliderTextWapper>
-                <h1>
-                    Projet Eltheria
-                </h1>
-                <img src="https://cdn.discordapp.com/attachments/538463475765215306/959851259937050714/Screenshot_2022-04-02_175309.png" alt="projet Eltheria"></img>
+                <SliderImg>
+                    <SliderTextTitle>
+                        Projet 1
+                    </SliderTextTitle>
+                </SliderImg>
             </SliderTextWapper>
         </SliderContainer>
     )
 }
 
 // TODO : faire un système pour rajouter des slides dans le composent app
+
 
 export default SlideOne
