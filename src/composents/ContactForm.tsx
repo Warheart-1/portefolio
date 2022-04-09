8457134bc7e7bbe2223aaecc2b03135bf61f97cf
Warheart-1 <axel97477@gmail.com>
2022-04-09 22:06:08 +0200

    ouais

--- src/composents/ContactForm.tsx
+++ src/composents/ContactForm.tsx
@@ -1,6 +1,7 @@
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react';
 import idEmailJS from '../data/idEmailJS';
+import swal from 'sweetalert2';
 
 export const  ContactForm = () => {
     const form = useRef(null);
@@ -11,8 +12,21 @@ export const  ContactForm = () => {
         emailjs.sendForm(idEmailJS.idService, idEmailJS.idTemplate, e.target, idEmailJS.idUser)
             .then((result) => {
                 console.log(result.text);
+                swal.fire({
+                    title: 'Merci !',
+                    text: 'Votre message a bien été envoyé !',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                })
             }, (error) => {
                 console.log(error.text);
+                swal.fire({
+                    title: 'Oups !',
+                    text: 'Une erreur est survenue, veuillez réessayer plus tard.',
+                    icon: 'error',
+                    confirmButtonText: 'Ok'
+                })
+
             });
         e.target.reset();
     };
@@ -21,15 +35,15 @@ export const  ContactForm = () => {
         <>
         <form ref={form} onSubmit={sendEmail}>
             <label htmlFor="Name">
-                <input type="text" name='from_name' placeholder='Your Name.' required></input>
+                <input type="text" name='from_name' placeholder='Votre nom.' required></input>
             </label>
             <label htmlFor="Email">
-                <input type="email" name='from_adresse' placeholder='Your Email.' required></input>
+                <input type="email" name='from_adresse' placeholder='Votre adresse mail.' required></input>
             </label>
             <label htmlFor="Message">
-                <textarea name='message' placeholder='Your Message.' required></textarea>
+                <textarea name='message' placeholder='Votre message.' required></textarea>
             </label>
-            <button type="submit" value="Send">Send</button>
+            <button type="submit" value="Send">Envoyé</button>
         </form>
         </>
     )
